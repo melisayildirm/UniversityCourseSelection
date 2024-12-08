fcadae79a9aa9d90a818ce3c011652b96b640d35
Melisa <melisaaayildirm@gmail.com>
2024-12-09 00:14:02 +0300

    Add files via upload

---
+++ Advisors.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityCourseSelection.Models
+{
+    public class Advisors
+    {
+        [Key]
+        public int AdvisorsID { get; set; }
+        public string FullName { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public ICollection<Students> Student { get; set; }
+    }
+}
---
+++ CoursesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityCourseSelection.Data;
+using UniversityCourseSelection.Models;
+
+namespace UniversityCourseSelection.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Courses>>> GetCourses()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Courses>> PostCourse(Courses course)
+        {
+            _context.Courses.Add(course);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetCourses", new { id = course.CourseID }, course);
+        }
+    }
+
+}
